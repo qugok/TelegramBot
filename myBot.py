0b9fe16190b4e710f1f87d8e9d12076ff9d74a74
qugok <iesekela@mail.ru>
2018-05-05 14:00:36 +0300

    незначительные изменения

--- myBot.py
+++ myBot.py
@@ -16,7 +16,7 @@ dispatcher = updater.dispatcher
 # Обработка команд
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text='Привет, давай пообщаемся?')
+                     text='Здравствуй, давай пообщаемся?')
 
 
 def textMessage(bot, update):
