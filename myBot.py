57bd65f910be5f433dde6206b41abfa8820e6e84
qugok <iesekela@mail.ru>
2018-05-08 16:14:31 +0300

    wiki from api change lang

--- myBot.py
+++ myBot.py
@@ -77,7 +77,7 @@ def textMessage(bot, update):
             # wikies[update.message.chat_id].find(wikies[update.message.chat_id].maybe[0])
             # print('code = ', code, ' text = ', wikies[update.message.chat_id].text)
             # bot.send_message(chat_id=update.message.chat_id, text=wikies[update.message.chat_id].text)
-            bot.send_message(chat_id=update.message.chat_id, text=str(wikies[update.message.chat_id].maybe))
+            bot.send_message(chat_id=update.message.chat_id, text=str(wikies[update.message.chat_id].maybe[:20]))
         log.write('message ' + wikies[update.message.chat_id].text + ' send to ' + name)
         log.write('message send\n')
         # print("message send")
